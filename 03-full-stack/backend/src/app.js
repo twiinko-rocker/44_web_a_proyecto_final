ebaccb43c0262cd4448b60fb31b07e655f404e0d
afsaanchez04 <afsanchez04@gmail.com>
2026-05-27 21:53:45 -0500

    implementación rutas en app.js

--- 03-full-stack/backend/src/app.js
+++ 03-full-stack/backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import { config } from 'dotenv'
+import alumnosRouter from '../routes/alumnos.routes.js'
 
 config()
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT
 
 app.use( express.json() )
 app.use( cors() )
+app.use( '/api/alumnos', alumnosRouter )
 
 mongoose.connect( process.env.MONGO_URI ).then( () => console.log("Conectado a MongoDB") )  
 
