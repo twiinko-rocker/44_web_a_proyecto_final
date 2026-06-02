bf91971025f1902f8592baef2fe945a746abace0
afsaanchez04 <afsanchez04@gmail.com>
2026-06-01 22:01:59 -0500

    jwt sign creado

--- 03-full-stack/backend/controllers/auth.controller.js
+++ 03-full-stack/backend/controllers/auth.controller.js
@@ -1,20 +1,46 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { config } from 'dotenv'
+config()
 
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
 
-    res.send(userSaved);
+    jwt.sign(
+      {
+        id: userSaved._id, //Payload
+      },
+      process.env.SECRET_KEY, //secret key
+      {
+        exiresIn: "1d", //duración
+      },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+
+        res.json({
+          id: userSaved._id,
+          username: userSaved.username,
+          email: userSaved.email,
+          createdAt: userSaved.createdAt,
+          updatedAt: userSaved.updatedAt,
+        });
+      },
+    );
   } catch (error) {
-    console.log(error);
+    res.status(500).json({message: error.message})
   }
 };
 
