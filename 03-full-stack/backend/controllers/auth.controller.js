050cbb74030ce8f7b2ce7eb4bf056b4e17fe2b82
afsaanchez04 <afsanchez04@gmail.com>
2026-06-01 21:02:45 -0500

    función register finalizada

--- 03-full-stack/backend/controllers/auth.controller.js
+++ 03-full-stack/backend/controllers/auth.controller.js
@@ -1,8 +1,23 @@
+import User from "../models/user.model.js";
 
-export const register = (req, res) => {
-  res.send("Registrando....")
-}
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    const userSaved = await newUser.save();
+
+    res.send(userSaved);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const login = (req, res) => {
-  res.send("Iniciando sesión...")
-}
+  res.send("Iniciando sesión...");
+};
