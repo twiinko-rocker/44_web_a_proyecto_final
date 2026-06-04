22a917155b1e0f932d2e368f6055ad235524eeae
afsaanchez04 <afsanchez04@gmail.com>
2026-06-03 21:25:15 -0500

    configuración origin cors en backend

--- 03-full-stack/backend/src/app.js
+++ 03-full-stack/backend/src/app.js
@@ -12,7 +12,10 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use( express.json() )
-app.use( cors() )
+app.use( cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}) )
 app.use( cookieParser() )
 app.use( '/api/alumnos', alumnosRouter )
 app.use( '/api', authRoutes )
--- 03-full-stack/frontend/src/api.js
+++ 03-full-stack/frontend/src/api.js
@@ -1,4 +1,7 @@
 
 import axios from "axios"
 
-export const api = axios.create( { baseURL: "http://localhost:3000/api/alumnos" } )
+export const api = axios.create( { 
+  baseURL: "http://localhost:3000/api" ,
+  withCredentials: true
+} )
